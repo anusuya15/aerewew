978db943a49c18c768718d275c53d848fcef4a29
anusuya15 <anusuyaindhu5@gmail.com>
2016-08-08 02:08:27 -0700

    dfhzrf
    sdgsgsrx

---
+++ re.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+
+public class Test3 {
+
+	public static void main(String[] args) {
+	
+
+	
+	 
+	
+	      String s, reverse = "";
+	      Scanner in = new Scanner(System.in);
+	 
+	      System.out.println("string to reverse");
+	      s = in.nextLine();
+	 
+	      int l = s.length();
+	 
+	      for ( int l = length - 1 ; l >= 0 ; l-- )
+                {
+	         reverse = reverse + original.charAt(l);
+	         }
+	      System.out.println("Reverse  string is: "+reverse);
+	   }
+	}
+	     
+
